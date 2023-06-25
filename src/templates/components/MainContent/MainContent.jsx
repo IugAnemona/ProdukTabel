95b5dcd5fd52e3a582c74bc67bf4557294c3db9b
IugAnemona <guilhermealvescoc@gmail.com>
2023-06-25 10:10:43 -0300

    adicionar o contexto nos componentes

--- src/templates/components/MainContent/MainContent.jsx
+++ src/templates/components/MainContent/MainContent.jsx
@@ -1,13 +1,16 @@
 import ProductsTable from "../ProductsTable/ProductsTable";
+import { TableProvider } from "../contexts/TableContext";
 
 const MainContent = () => {
   return (
     <div className="my-10">
-      <div className="flex flex-wrap w-screen justify-center p-5 h-full">
-        <h1 className="text-4xl font-bold text-davysgray p-5 my-5">
+      <div className="flex flex-wrap w-screen justify-center p-2 md:p-5 h-full">
+        <h1 className="text-4xl w-full text-center font-bold text-davysgray p-5 my-5">
           Tabela de Produtos
         </h1>
-        <ProductsTable />
+        <TableProvider>
+          <ProductsTable />
+        </TableProvider>
       </div>
     </div>
   );
