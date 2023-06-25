6b019792e397662a7c9e3f121b3f8822b1a81b99
IugAnemona <guilhermealvescoc@gmail.com>
2023-06-25 10:12:40 -0300

    adicionar contato com a api com delete e get

--- src/templates/components/ProductsTable/ProductsTable.jsx
+++ src/templates/components/ProductsTable/ProductsTable.jsx
@@ -1,19 +1,26 @@
 import { BsFillTrashFill } from "react-icons/bs";
 import AddProduct from "../AddProduct/AddProduct";
-import productsData from "../../Api/ApiData";
-import { useEffect, useState } from "react";
+import { useContext, useEffect } from "react";
+import { TableContext } from "../contexts/TableContext";
+import { deleteProduct, getProducts } from "../../Api/products/";
+import { CgSpinner } from "react-icons/cg";
 
 const ProductsTable = () => {
-  const [products, setProducts] = useState(null);
+  const { search, products, setProducts } = useContext(TableContext);
 
-  async function aaa() {
-    const products = await productsData();
+  async function getTable() {
+    const products = await getProducts();
     setProducts(products);
-    console.log("oi");
+  }
+
+  async function deleteP(id) {
+    await deleteProduct(id);
+    const products = await getProducts();
+    setProducts(products);
   }
 
   useEffect(() => {
-    aaa();
+    getTable();
   }, []);
 
   return (
@@ -22,7 +29,7 @@ const ProductsTable = () => {
       <div className="my-8 shadow-sm bg-aliceblue">
         <div className="table w-full border-collapse table-auto text-base lg:text-lg">
           <div className="table-header-group">
-            <div className="table-row text-base lg:text-xl">
+            <div className="table-row text-base lg:text-lg">
               <div className="table-cell text-left font-medium p-2 pl-8 pt-0 pb-3 text-powderblue">
                 Produto
               </div>
@@ -35,38 +42,62 @@ const ProductsTable = () => {
               <div className="table-cell text-left font-medium p-2 pl-8 pt-0 pb-3 text-powderblue"></div>
             </div>
           </div>
+
           {products ? (
-            products.map((p) => (
-              <div key={p.id} className="table-row-group bg-slate-50">
-                <div className="table-row">
-                  <div className="table-cell border-b border-slate-100 p-4 pl-8 lg:p-14 lg:pl-8 text-davysgray">
-                    {p.nome_produto}
-                  </div>
-                  <div className="table-cell border-b border-slate-100 p-4 pl-8 lg:p-14 lg:pl-8 text-davysgray">
-                    {p.quantidade}
-                    <a
-                      className="mx-2 text-lg font-bold text-green-400"
-                      href="#"
-                    >
-                      +
-                    </a>
-                    |
-                    <a className="mx-2 text-lg font-bold text-red-400" href="#">
-                      -
-                    </a>
-                  </div>
-                  <div className="table-cell border-b border-slate-100 p-4 pl-8 lg:p-14 lg:pl-8 text-slate-500">
-                    {p.valor}
-                  </div>
-                  <div className="table-cell border-b border-slate-100 p-4 pl-8 lg:p-14 lg:pl-8 text-slate-500 text-lg">
-                    <BsFillTrashFill className="hover:scale-125 cursor-pointer" />
+            products
+              .filter((p) =>
+                p.nome_produto.toLowerCase().includes(search.toLowerCase())
+              )
+              .map((p) => (
+                <div key={p.id} className="table-row-group bg-slate-50">
+                  <div className="table-row">
+                    <div className="table-cell border-b border-slate-100 p-4 pl-8  text-davysgray">
+                      {p.nome_produto}
+                    </div>
+                    <div className="table-cell border-b border-slate-100 p-4 pl-8  text-davysgray">
+                      {p.quantidade}
+                      <a
+                        className="mx-2 text-lg font-bold text-green-400"
+                        href="#"
+                      >
+                        +
+                      </a>
+                      |
+                      <a
+                        className="mx-2 text-lg font-bold text-red-400"
+                        href="#"
+                      >
+                        -
+                      </a>
+                    </div>
+                    <div className="table-cell border-b border-slate-100 p-4 pl-8  text-slate-500">
+                      {p.valor}
+                    </div>
+                    <div className="table-cell border-b border-slate-100 p-4 pl-8  text-slate-500 text-lg">
+                      <BsFillTrashFill
+                        onClick={() => deleteP(p.id)}
+                        className="hover:scale-125 cursor-pointer"
+                      />
+                    </div>
                   </div>
                 </div>
-              </div>
-            ))
+              ))
           ) : (
-            <h1>loading...</h1>
+            <div className="w-full flex justify-end text-5xl p-5">
+              <CgSpinner className="animate-spin" />
+            </div>
           )}
+          {products &&
+            products.length > 0 &&
+            products.filter((p) =>
+              p.nome_produto.toLowerCase().includes(search.toLowerCase())
+            ).length === 0 && (
+              <div className="p-6 text-xl text-davysgray">
+                <h2 className="">
+                  Nenhum produto encontrado com o nome `{search}`
+                </h2>
+              </div>
+            )}
         </div>
       </div>
     </div>
