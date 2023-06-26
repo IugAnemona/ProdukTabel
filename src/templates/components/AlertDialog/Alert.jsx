3cb8d6a6642ba0454a032a42176b4e68f46a0a03
IugAnemona <guilhermealvescoc@gmail.com>
2023-06-26 12:21:25 -0300

    atualizar texto de alerta

--- src/templates/components/AlertDialog/Alert.jsx
+++ src/templates/components/AlertDialog/Alert.jsx
@@ -16,11 +16,11 @@ const Alert = (props) => (
       <AlertDialog.Overlay className="bg-blackA9 data-[state=open]:animate-overlayShow fixed inset-0" />
       <AlertDialog.Content className="data-[state=open]:animate-contentShow fixed top-[50%] left-[50%] max-h-[85vh] w-[90vw] max-w-[500px] translate-x-[-50%] translate-y-[-50%] rounded-[6px] bg-white p-[25px] shadow-[hsl(206_22%_7%_/_35%)_0px_10px_38px_-10px,_hsl(206_22%_7%_/_20%)_0px_10px_20px_-15px] focus:outline-none">
         <AlertDialog.Title className="text-mauve12 m-0 text-[17px] font-medium">
-          Are you absolutely sure?
+          Você tem certeza?
         </AlertDialog.Title>
         <AlertDialog.Description className="text-mauve11 mt-4 mb-5 text-[15px] leading-normal">
-          This action cannot be undone. This will permanently delete your
-          account and remove your data from our servers.
+          Essa ação não pode ser desfeita. Isso vai apagar permanentemente seu
+          produto.
         </AlertDialog.Description>
         <div className="flex justify-end gap-[25px]">
           <AlertDialog.Cancel asChild>
