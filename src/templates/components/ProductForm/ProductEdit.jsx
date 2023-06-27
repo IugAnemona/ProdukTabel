8c3b6ecab48381fe4be1844f83eef30ec52208ba
IugAnemona <guilhermealvescoc@gmail.com>
2023-06-27 11:34:15 -0300

    adicionar validação dos campos

--- src/templates/components/ProductForm/ProductEdit.jsx
+++ src/templates/components/ProductForm/ProductEdit.jsx
@@ -1,16 +1,16 @@
 import React, { useContext, useState } from "react";
 import * as Dialog from "@radix-ui/react-dialog";
-import { AiOutlinePlus } from "react-icons/ai";
 import { GrFormClose } from "react-icons/gr";
 import { TableContext } from "../contexts/TableContext";
-import { getProducts, createProduct } from "../../Api/products";
+import { getProducts, updateProduct } from "../../Api/products";
+import { BsPencilSquare } from "react-icons/bs";
+import PropTypes from "prop-types";
 import { useForm } from "react-hook-form";
 import { yupResolver } from "@hookform/resolvers/yup";
 import * as yup from "yup";
 
-const ProductForm = () => {
+const ProductEdit = (props) => {
   const [open, setOpen] = useState(false);
-
   const { setProducts } = useContext(TableContext);
 
   const validationYup = yup.object().shape({
@@ -18,6 +18,7 @@ const ProductForm = () => {
     quantidade: yup.string().required("Quantidade é obrigatorio"),
     valor: yup.string().required("Preço é obrigatório"),
   });
+
   const {
     register,
     handleSubmit,
@@ -32,9 +33,14 @@ const ProductForm = () => {
       quantidade: Number(values.quantidade),
       valor: Number(values.valor),
     };
-    postNewProduct();
+    updateP(addNew, props.products.id);
     setOpen(false);
   };
+
+  async function updateP(data, id) {
+    await updateProduct(data, id);
+    getNewTable();
+  }
 
   let addNew = {};
 
@@ -43,27 +49,24 @@ const ProductForm = () => {
     setProducts(products);
   }
 
-  const postNewProduct = async () => {
-    await createProduct(addNew);
-    getNewTable();
-  };
-
   return (
     <Dialog.Root open={open} onOpenChange={setOpen}>
       <Dialog.Trigger asChild>
-        <button className="px-2 py-2 md:px-4 md:py-2 text-white  bg-emerald hover:bg-green-500 rounded-md font-bold text-base md:text-xl  cursor-pointer active:bg-seagreen active:text-davysgray">
-          <AiOutlinePlus className="inline pb-1 text-2xl" />
-          <div className="hidden md:inline">Produto</div>
+        <button className="text-violet11 hover:bg-mauve3 mr-4 shadow-blackA7 inline-flex p-1 items-center justify-center rounded-[4px] bg-white font-medium leading-none shadow-[0_2px_10px] outline-none">
+          <BsPencilSquare
+            // onClick={() => deleteP(p.id)}
+            className="hover:scale-125 cursor-pointer"
+          />
         </button>
       </Dialog.Trigger>
       <Dialog.Portal>
         <Dialog.Overlay className="fixed " />
         <Dialog.Content className="text-slate-700 font-ubuntu font-light bg-powderblue rounded-md shadow-2xl fixed top-2/4 left-2/4 -translate-y-1/2 -translate-x-1/2 w-11/12 max-w-md max-h-[85vh] p-6">
           <Dialog.Title className="m-0 font-medium text-lg ">
-            Novo Produto
+            Editar Produto
           </Dialog.Title>
           <Dialog.Description className="mt-2 mb-5 text-base">
-            Adicione um novo produto.
+            Edite o produto escolhido.
           </Dialog.Description>
           <form onSubmit={handleSubmit(onSubmit)}>
             <fieldset className="flex flex-wrap gap-5 items-center mb-4">
@@ -75,6 +78,7 @@ const ProductForm = () => {
               </label>
               <input
                 name="nome_produto"
+                defaultValue={props.products.nome_produto}
                 {...register("nome_produto")}
                 className="p-2 border-2 outline-0 border-border max-w-xl w-full flex-1 shadow-sm rounded-md text-base focus:border-focus focus:shadow-sm text-text"
               />
@@ -90,6 +94,7 @@ const ProductForm = () => {
                 Quantidade
               </label>
               <input
+                defaultValue={props.products.quantidade}
                 name="quantidade"
                 {...register("quantidade")}
                 className="p-2 border-2 outline-0 border-border max-w-xl w-full flex-1 shadow-sm rounded-md text-base focus:border-focus focus:shadow-sm text-text"
@@ -103,11 +108,13 @@ const ProductForm = () => {
                 Preço
               </label>
               <input
+                // onChange={(e) => setValorInicial(e.target.value)}
+                defaultValue={props.products.valor}
                 name="valor"
                 {...register("valor")}
                 className="p-2 border-2 outline-0 border-border max-w-xl w-full flex-1 shadow-sm rounded-md text-base focus:border-focus focus:shadow-sm text-text"
               />
-              <p className="text-red-500 w-full text-end flex-wrap">
+              <p className="text-red-500 w-full text-end">
                 {errors.valor?.message}
               </p>
             </fieldset>
@@ -116,7 +123,7 @@ const ProductForm = () => {
                 type="submit"
                 className="inline-flex items-center justify-center rounded py-0 px-4 text-base font-medium h-9 bg-aliceblue text-davysgray hover:bg-blue-200"
               >
-                Adicionar
+                Salvar
               </button>
             </div>
           </form>
@@ -141,4 +148,9 @@ const ProductForm = () => {
   );
 };
 
-export default ProductForm;
+ProductEdit.propTypes = {
+  update: PropTypes.func,
+  products: PropTypes.object,
+};
+
+export default ProductEdit;
